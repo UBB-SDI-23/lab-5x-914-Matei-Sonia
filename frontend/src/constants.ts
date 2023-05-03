4128371b37f5a35675ae518295358bb327934b6c
914-Matei-Sonia <sonia.matei1@stud.ubbcluj.ro>
2023-05-03 10:34:21 +0300

    prod backend

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
 const PROD_BACKEND_API_URL = "http://16.16.170.17/passwords";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/passwords";
 
-export const BACKEND_API_URL = DEV_BACKEND_API_URL;
+export const BACKEND_API_URL = PROD_BACKEND_API_URL;
