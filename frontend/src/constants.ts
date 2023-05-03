cfa2478e60309c21acc43f96b285ec8c54a195b8
914-Matei-Sonia <sonia.matei1@stud.ubbcluj.ro>
2023-05-03 10:40:11 +0300

    afraid link

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-const PROD_BACKEND_API_URL = "http://16.16.170.17/passwords";
+const PROD_BACKEND_API_URL = "http://password-app.ugli.se/";
 const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/passwords";
 
 export const BACKEND_API_URL = PROD_BACKEND_API_URL;
